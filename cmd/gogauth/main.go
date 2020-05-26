5f6c7bd4057b098d930058f62efe5e3f900bb812
Nik V <github@nikvdp.com>
2020-05-25 19:07:27 -0700

    🍴 Switch off of my sio fork, cleanups

--- cmd/gogauth/main.go
+++ cmd/gogauth/main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	// "encoding/json"
-	sio "bitbucket.org/nikvdp/sia-ncrypt"
-	// ncrypt "bitbucket.org/nikvdp/sia-ncrypt/cmd/ncrypt"
+	"github.com/minio/sio"
 	"encoding/json"
 	"fmt"
 	"github.com/pquerna/otp/totp"
 	"golang.org/x/crypto/scrypt"
 	"io"
-	// "io/ioutil"
 	"os"
-	// "os/exec"
 	"bufio"
 	"bytes"
 	"encoding/base64"
@@ -47,7 +43,7 @@ func doDecrypt() string {
 	} else if _, err := os.Stat(fmt.Sprint("../../", filename)); err == nil {
 		absFilename, _ = filepath.Abs(fmt.Sprint("../../", filename))
 	}
-	// inputData, err := ioutil.ReadFile(filename)
+
 	origInFile, err := os.Open(absFilename)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, fmt.Sprintf("Couldn't open file '%s'", absFilename))
@@ -63,32 +59,17 @@ func doDecrypt() string {
 
 	key, err := scrypt.Key([]byte(pw), salt, 32768, 16, 1, 32)
 
-	// ncryptPath := "/home/nik/.vim/bundle/nikcrypt/ncrypt.Linux"
+
 	sioConfig := sio.Config{Key: key}
 	var buff bytes.Buffer
 	decrypted := bufio.NewWriter(&buff)
 	_, err = sio.Decrypt(decrypted, inFile, sioConfig)
-	// bytesWritten, err := sio.Decrypt(os.Stdout, inFile, sioConfig)
+
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Decryption error!", err)
 		os.Exit(3)
 	}
 
-	// decryptCmd := exec.Command(ncryptPath, "-d", "-b", "-p", pw)
-
-	// decryptIn, _ := decryptCmd.StdinPipe()
-	// decryptOut, _ := decryptCmd.StdoutPipe()
-	// decryptErr, _ := decryptCmd.StderrPipe()
-
-	// decryptCmd.Start()
-	// decryptIn.Write(inputData)
-	// decryptIn.Close()
-	// decryptBytes, _ := ioutil.ReadAll(decryptOut)
-	// decryptErrBytes, _ := ioutil.ReadAll(decryptErr)
-	// decryptCmd.Wait()
-	// decryptedStr := string(decryptBytes)
-	// block("bytes", decryptedString)
-	// block("ERROR", string(decryptErrBytes))
 	decrypted.Flush()
 	return buff.String()
 }
--- go.mod
+++ go.mod
@@ -3,11 +3,7 @@ module github.com/nikvdp/gogauth
 go 1.14
 
 require (
-	bitbucket.org/nikvdp/sia-ncrypt v0.0.0-20190209075517-761241b0bd71
-	//	github.com/minio/sio v0.2.0 // indirect
+	github.com/minio/sio v0.2.0
 	github.com/pquerna/otp v1.2.0
 	golang.org/x/crypto v0.0.0-20181106171534-e4dc69e5b2fd
 )
-
-// replace  bitbucket.org/nikvdp/sia-ncrypt => github.com/minio/sio v0.2.0
-replace bitbucket.org/nikvdp/sia-ncrypt => /home/nik/Code/personal/go/Linux/src/bitbucket.org/nikvdp/sia-ncrypt/nothing
