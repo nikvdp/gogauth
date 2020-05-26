6bac5d75913aa6b1938163764b1021c0675c3235
Nik V <github@nikvdp.com>
2020-05-25 18:45:43 -0700

    🔒 decypt via ncrypt lib directly (no cli)

--- cmd/gogauth/main.go
+++ cmd/gogauth/main.go
@@ -2,59 +2,95 @@ package main
 
 import (
 	// "encoding/json"
+	sio "bitbucket.org/nikvdp/sia-ncrypt"
+	// ncrypt "bitbucket.org/nikvdp/sia-ncrypt/cmd/ncrypt"
 	"encoding/json"
 	"fmt"
 	"github.com/pquerna/otp/totp"
-	"io/ioutil"
+	"golang.org/x/crypto/scrypt"
+	"io"
+	// "io/ioutil"
 	"os"
-	"os/exec"
+	// "os/exec"
+	"bufio"
+	"bytes"
+	"encoding/base64"
+	"path/filepath"
 	"regexp"
 	"strings"
+	"text/tabwriter"
 	"time"
 )
 
 func main() {
-	fmt.Print("hey again\n")
 	decrypted := doDecrypt()
-	var totpKeys map[string]interface{}
+	var totpKeys map[string]string
 	json.Unmarshal([]byte(decrypted), &totpKeys)
 	re, _ := regexp.Compile("[ -]+")
+	writer := tabwriter.NewWriter(os.Stdout, 2, 1, 3, ' ', 0)
 	for key, val := range totpKeys {
 		keyStr := fmt.Sprintf("%s", val)
 		keyStr = strings.ToUpper(re.ReplaceAllString(keyStr, ""))
 
 		code, _ := totp.GenerateCode(keyStr, time.Now())
-		fmt.Printf("%s = %s\n", key, code)
+		fmt.Fprintf(writer, "%s\t%s\n", key, code)
 	}
-	// fmt.Println("Unmarshalled: ", totpKeys)
+	writer.Flush()
 }
 
 func doDecrypt() string {
-	filename := "./auth_keys.json.ncrypt"
-	file, err := ioutil.ReadFile(filename)
+	pw := os.Getenv("DECRYPT_PASSWORD")
+	filename := "auth_keys.json.ncrypt"
+	var absFilename string
+	if _, err := os.Stat(fmt.Sprint("./", filename)); err == nil {
+		absFilename, _ = filepath.Abs(fmt.Sprint("./", filename))
+	} else if _, err := os.Stat(fmt.Sprint("../../", filename)); err == nil {
+		absFilename, _ = filepath.Abs(fmt.Sprint("../../", filename))
+	}
+	// inputData, err := ioutil.ReadFile(filename)
+	origInFile, err := os.Open(absFilename)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Something went wrong!")
+		fmt.Fprintln(os.Stderr, fmt.Sprintf("Couldn't open file '%s'", absFilename))
 		os.Exit(1)
 	}
+	inFile := base64.NewDecoder(base64.StdEncoding, origInFile)
 
-	ncryptPath := "/home/nik/.vim/bundle/nikcrypt/ncrypt.Linux"
-	pw := os.Getenv("DECRYPT_PASSWORD")
-	decryptCmd := exec.Command(ncryptPath, "-d", "-b", "-p", pw)
-	// decryptCmd := exec.Command("cat")
-	decryptIn, _ := decryptCmd.StdinPipe()
-	decryptOut, _ := decryptCmd.StdoutPipe()
+	salt := make([]byte, 32)
+	if _, err := io.ReadFull(inFile, salt); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to read salt from '%s'\n", filename)
+		os.Exit(2)
+	}
+
+	key, err := scrypt.Key([]byte(pw), salt, 32768, 16, 1, 32)
+
+	// ncryptPath := "/home/nik/.vim/bundle/nikcrypt/ncrypt.Linux"
+	sioConfig := sio.Config{Key: key}
+	var buff bytes.Buffer
+	decrypted := bufio.NewWriter(&buff)
+	_, err = sio.Decrypt(decrypted, inFile, sioConfig)
+	// bytesWritten, err := sio.Decrypt(os.Stdout, inFile, sioConfig)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Decryption error!", err)
+		os.Exit(3)
+	}
+
+	// decryptCmd := exec.Command(ncryptPath, "-d", "-b", "-p", pw)
+
+	// decryptIn, _ := decryptCmd.StdinPipe()
+	// decryptOut, _ := decryptCmd.StdoutPipe()
 	// decryptErr, _ := decryptCmd.StderrPipe()
 
-	decryptCmd.Start()
-	decryptIn.Write(file)
-	decryptIn.Close()
-	decryptBytes, _ := ioutil.ReadAll(decryptOut)
+	// decryptCmd.Start()
+	// decryptIn.Write(inputData)
+	// decryptIn.Close()
+	// decryptBytes, _ := ioutil.ReadAll(decryptOut)
 	// decryptErrBytes, _ := ioutil.ReadAll(decryptErr)
-	decryptCmd.Wait()
-	decryptedStr := string(decryptBytes)
+	// decryptCmd.Wait()
+	// decryptedStr := string(decryptBytes)
 	// block("bytes", decryptedString)
 	// block("ERROR", string(decryptErrBytes))
-	return decryptedStr
+	decrypted.Flush()
+	return buff.String()
 }
 
 func block(caption string, text string) {
--- go.mod
+++ go.mod
@@ -3,20 +3,11 @@ module github.com/nikvdp/gogauth
 go 1.14
 
 require (
-//	github.com/minio/sio v0.2.0 // indirect
+	bitbucket.org/nikvdp/sia-ncrypt v0.0.0-20190209075517-761241b0bd71
+	//	github.com/minio/sio v0.2.0 // indirect
 	github.com/pquerna/otp v1.2.0
-    bitbucket.org/nikvdp/sia-ncrypt 761241b0bd71dd608f3789bcfe2c27806ad9f780
+	golang.org/x/crypto v0.0.0-20181106171534-e4dc69e5b2fd
 )
 
-// require (
-//     bitbucket.org/nikvdp/sia-ncrypt 761241b0bd71dd608f3789bcfe2c27806ad9f780
-// )
-//
-// replace  bitbucket.org/nikvdp/sia-ncrypt => github.com/minio/sio 761241b0bd71dd608f3789bcfe2c27806ad9f780
-
-// require (
-//         bitbucket.org/nikvdp/sia-ncrypt v0.0.0-20190401055205-025f82420c42
-// )
-// from `go get`
 // replace  bitbucket.org/nikvdp/sia-ncrypt => github.com/minio/sio v0.2.0
-replace  bitbucket.org/nikvdp/sia-ncrypt => /home/nik/Code/personal/go/Linux/src/bitbucket.org/nikvdp/sia-ncrypt/nothing
+replace bitbucket.org/nikvdp/sia-ncrypt => /home/nik/Code/personal/go/Linux/src/bitbucket.org/nikvdp/sia-ncrypt/nothing
