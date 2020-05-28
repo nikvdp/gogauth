1ddc04c6127a4ff7f2dddde186eaca4c8c7162ef
Nik V <github@nikvdp.com>
2020-05-27 19:03:13 -0700

    🧸 finish add command

--- cmd/gogauth/main.go
+++ cmd/gogauth/main.go
@@ -19,11 +19,65 @@ import (
 	"time"
 )
 
-func main() {
-	displayDecryptedKeys()
+var keyCleanRegex, _ = regexp.Compile("[ -]+")
+
+var TOTPKEYS_FILE = "auth_keys.json.ncrypt"
+
+var rootCmd = &cobra.Command{
+	Use:   "gogauth",
+	Short: "google auth compatible cli",
+	Long:  "Long google auth compatible cli",
+	Run: func(cmd *cobra.Command, args []string) {
+		decryptAndDisplay()
+	},
 }
 
-func displayDecryptedKeys() {
+func main() {
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "list",
+		Short: "show codes",
+		Run: func(cmd *cobra.Command, args []string) {
+			decryptAndDisplay()
+		},
+	})
+
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "add",
+		Short: "add a new code",
+		Args:  cobra.MinimumNArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			// fmt.Println("Add a key")
+			// fmt.Println("arg 1: ", args[0], " arg 2: ", args[1])
+			addKey(args[0], args[1])
+		},
+	})
+
+	rootCmd.Execute()
+	// displayDecryptedKeys()
+}
+
+func verifyTotpIsValid(key string) bool {
+	_, err := totp.GenerateCode(key, time.Now())
+	return err == nil
+}
+
+func addKey(name string, totp string) {
+	totpClean := cleanTotpKey(totp)
+	totpKeys := doDecrypt()
+
+	if !verifyTotpIsValid(totpClean) {
+		fmt.Fprintf(os.Stderr, "Key '%s' doesn't seem to be a valid totp key!\n", totp)
+		os.Exit(1)
+	}
+
+	totpKeys[name] = totpClean
+	jsoned, _ := json.MarshalIndent(totpKeys, "", "    ")
+	fmt.Printf("Added key '%s' with code '%s' to db!", name, totpClean)
+	fmt.Println("New json: ")
+	fmt.Println(string(jsoned))
+}
+
+func decryptAndDisplay() {
 	decrypted := cleanTotpKeys(doDecrypt())
 	writer := tabwriter.NewWriter(os.Stdout, 2, 1, 3, ' ', 0)
 	for key, val := range decrypted {
@@ -35,12 +89,11 @@ func displayDecryptedKeys() {
 
 func doDecrypt() map[string]string {
 	pw := os.Getenv("DECRYPT_PASSWORD")
-	filename := "auth_keys.json.ncrypt"
 	var absFilename string
-	if _, err := os.Stat(fmt.Sprint("./", filename)); err == nil {
-		absFilename, _ = filepath.Abs(fmt.Sprint("./", filename))
-	} else if _, err := os.Stat(fmt.Sprint("../../", filename)); err == nil {
-		absFilename, _ = filepath.Abs(fmt.Sprint("../../", filename))
+	if _, err := os.Stat(fmt.Sprint("./", TOTPKEYS_FILE)); err == nil {
+		absFilename, _ = filepath.Abs(fmt.Sprint("./", TOTPKEYS_FILE))
+	} else if _, err := os.Stat(fmt.Sprint("../../", TOTPKEYS_FILE)); err == nil {
+		absFilename, _ = filepath.Abs(fmt.Sprint("../../", TOTPKEYS_FILE))
 	}
 
 	origInFile, err := os.Open(absFilename)
@@ -52,7 +105,7 @@ func doDecrypt() map[string]string {
 
 	salt := make([]byte, 32)
 	if _, err := io.ReadFull(inFile, salt); err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to read salt from '%s'\n", filename)
+		fmt.Fprintf(os.Stderr, "Failed to read salt from '%s'\n", TOTPKEYS_FILE)
 		os.Exit(2)
 	}
 
@@ -75,15 +128,17 @@ func doDecrypt() map[string]string {
 	return totpKeys
 }
 
+func cleanTotpKey(totpKey string) string {
+	return strings.ToUpper(keyCleanRegex.ReplaceAllString(totpKey, ""))
+}
+
 func cleanTotpKeys(totpKeys map[string]string) map[string]string {
 	// this totp lib doesn't like
-	re, _ := regexp.Compile("[ -]+")
 	cleanedKeys := make(map[string]string)
 	for key, val := range totpKeys {
 		valStr := fmt.Sprintf("%s", val)
 		// tquerna/otp requires uppercased values
-		valStr = strings.ToUpper(re.ReplaceAllString(valStr, ""))
-		cleanedKeys[key] = valStr
+		cleanedKeys[key] = cleanTotpKey(valStr)
 	}
 
 	return cleanedKeys
--- go.mod
+++ go.mod
@@ -5,5 +5,11 @@ go 1.14
 require (
 	github.com/minio/sio v0.2.0
 	github.com/pquerna/otp v1.2.0
-	golang.org/x/crypto v0.0.0-20181106171534-e4dc69e5b2fd
+	github.com/spf13/cobra v1.0.0
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37
+	golang.org/x/sys v0.0.0-20200523222454-059865788121 // indirect
 )
+
+// replace  bitbucket.org/nikvdp/sia-ncrypt => github.com/minio/sio v0.2.0
+// replace bitbucket.org/nikvdp/sia-ncrypt => /home/nik/Code/personal/go/Linux/src/bitbucket.org/nikvdp/sia-ncrypt
